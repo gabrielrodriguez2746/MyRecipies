eef160d1d1c3845a77ab0cc44b9cdadb199544db
Gabriel Rodriguez <gabriel.rodriguez3103@gmail.com>
2019-07-21 14:45:38 -0500

    :clapper: initial commit

---
+++ app/src/main/java/com/myrecipes/RecipesApplication.java
@@ -0,0 +1,21 @@
+package com.myrecipes;
+
+import android.app.Activity;
+import android.app.Application;
+
+import javax.inject.Inject;
+
+import dagger.android.AndroidInjector;
+import dagger.android.DispatchingAndroidInjector;
+import dagger.android.HasActivityInjector;
+
+public class RecipesApplication extends Application implements HasActivityInjector {
+
+    @Inject
+    DispatchingAndroidInjector<Activity> activityDispatchingAndroidInjector;
+
+    @Override
+    public AndroidInjector<Activity> activityInjector() {
+        return activityDispatchingAndroidInjector;
+    }
+}
