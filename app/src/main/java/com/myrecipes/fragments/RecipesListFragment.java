2cca0aa8fe901cd4f1d8df9b3bb1bfd02fc41eb6
gabrielrodriguez2746 <gabriel.rodriguez3103@gmail.com>
2019-08-05 22:29:52 -0500

    :lipstick: Feature | recipe items ui | Gabriel Rodriguez (#4)

--- app/src/main/java/com/myrecipes/fragments/RecipesListFragment.java
+++ app/src/main/java/com/myrecipes/fragments/RecipesListFragment.java
@@ -2,6 +2,7 @@ package com.myrecipes.fragments;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,22 +13,31 @@ import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.myrecipes.R;
+import com.myrecipes.adapters.RecipesListAdapter;
 import com.myrecipes.data.models.Recipe;
 import com.myrecipes.databinding.FragmentRecipesListBinding;
+import com.myrecipes.decorators.VerticalSpaceItemDecoration;
+import com.myrecipes.models.RecyclerViewConfiguration;
 import com.myrecipes.viewmodels.main.RecipesListViewModel;
 
+import org.jetbrains.annotations.NotNull;
+
 import java.util.List;
+import java.util.Objects;
 
 import javax.inject.Inject;
 
 import dagger.android.support.AndroidSupportInjection;
 import timber.log.Timber;
 
-public class RecipesListFragment extends Fragment {
+public class RecipesListFragment extends Fragment implements RecipesListAdapter.OnRecipeClicked {
 
     private FragmentRecipesListBinding binding;
+    private RecipesListAdapter adapter;
     private RecipesListViewModel viewModel;
 
     @Inject
@@ -43,7 +53,12 @@ public class RecipesListFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         if (binding == null) {
+            DisplayMetrics displayMetrics = getDisplayMetrics();
+            adapter = new RecipesListAdapter(displayMetrics, this);
+            RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getContext());
             binding = DataBindingUtil.inflate(inflater, R.layout.fragment_recipes_list, container, false);
+            binding.setRecyclerConfiguration(new RecyclerViewConfiguration(layoutManager, adapter,
+                    new VerticalSpaceItemDecoration((getResources().getDimensionPixelSize(R.dimen.space_small)))));
         }
         return binding != null ? binding.getRoot() : super.onCreateView(inflater, container, savedInstanceState);
     }
@@ -58,7 +73,20 @@ public class RecipesListFragment extends Fragment {
         viewModel.getItemsLiveData().observe(getViewLifecycleOwner(), this::processItems);
     }
 
+    @NotNull
+    private DisplayMetrics getDisplayMetrics() {
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        Objects.requireNonNull(getActivity()).getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        return displayMetrics;
+    }
+
     private void processItems(List<Recipe> recipes) {
         Timber.d("recipes at fragment :: %s", recipes.toString());
+        adapter.submitList(recipes);
+    }
+
+    @Override
+    public void onRecipeClicked(int recipeId) {
+
     }
 }
