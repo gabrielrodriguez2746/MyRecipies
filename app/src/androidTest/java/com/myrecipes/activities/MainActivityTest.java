10af643ea81852ce70fcb662f1a67e13c3a00607
Gabriel Rodriguez <gabriel.rodriguez3103@gmail.com>
2019-09-01 14:45:15 -0500

    :star: Ui test with espresso

---
+++ app/src/androidTest/java/com/myrecipes/activities/MainActivityTest.java
@@ -0,0 +1,82 @@
+package com.myrecipes.activities;
+
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.test.core.app.ActivityScenario;
+import androidx.test.espresso.Espresso;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import com.myrecipes.R;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.not;
+
+@RunWith(AndroidJUnit4.class)
+public class MainActivityTest {
+
+    private ActivityScenario scenario;
+
+    @Before
+    public void setup() {
+        scenario = ActivityScenario.launch(MainActivity.class);
+    }
+
+    @Test
+    public void validateE2EWorkflow() {
+        Espresso.onView(withText("My Recipes")).check(matches(isDisplayed()));
+        waitForService(500);
+        Espresso.onView(withText("NUTELLA PIE")).check(matches(isDisplayed()));
+        Espresso.onView(withText("NUTELLA PIE")).perform(click());
+        waitForService(400);
+        Espresso.onView(withId(R.id.tv_ingredients)).check(matches(not(withText(""))));
+        Espresso.onView(nthChildOf(withId(R.id.ll_steps), 0)).perform(click());
+        waitForService(400);
+        Espresso.onView(withId(R.id.pvRecipe)).check(matches(isDisplayed()));
+    }
+
+    private void waitForService(int time) {
+        try {
+            Thread.sleep(time);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @After
+    public void tearDown() {
+        scenario.close();
+    }
+
+    public static Matcher<View> nthChildOf(final Matcher<View> parentMatcher, final int childPosition) {
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("with "+childPosition+" child view of type parentMatcher");
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                if (!(view.getParent() instanceof ViewGroup)) {
+                    return parentMatcher.matches(view.getParent());
+                }
+
+                ViewGroup group = (ViewGroup) view.getParent();
+                return parentMatcher.matches(view.getParent()) && group.getChildAt(childPosition).equals(view);
+            }
+        };
+    }
+
+}
